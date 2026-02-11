826e54b679901988c246e7b6d74b0ea3a164db00
Vinothhk <iamvinothkumar16@gmail.com>
2026-02-11 21:40:16 +0530

    init commit

---
+++ visualize_gripper.py
@@ -0,0 +1,13 @@
+import mujoco
+import mujoco.viewer
+import numpy as np
+
+# Load your XML file
+model = mujoco.MjModel.from_xml_path("gripper.mujoco.xml")
+data = mujoco.MjData(model)
+
+# Launch interactive viewer
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
